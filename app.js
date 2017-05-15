d9d190350bcf6970fd761f3ce162ebb854fcd59e
Carson777 <carsoncapper@gmail.com>
2017-05-15 18:00:21 -0500

    Combined jplayer with vue

---
+++ app.js
@@ -0,0 +1,49 @@
+var vm = new Vue({
+	el: '#app',
+  data: {
+  	button: 'play',
+    started: 'false',
+    play: 'https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-play-128.png',
+    pause: 'https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-pause-128.png',
+    url: 'https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-play-128.png',
+    progressBar: {
+    	width: '0px',
+      backgroundColor: 'red'
+    },
+    time: time,
+    duration: duration,
+    timeRemaining: timeRemaining
+  },
+  methods: {
+    foo: function () {
+    	if (this.started === 'false'){
+    		var that = this;
+    		setInterval(function () {
+    			that.time = time;
+    			that.timeRemaining = timeRemaining;
+    		}, 1000);
+    	}
+    	if (this.button === 'play'){
+    		this.button = 'pause';
+    		this.url = this.pause;
+    	} else if (this.button === 'pause'){
+    		this.button = 'play';
+    		this.url = this.play;
+    	}
+    	this.started = true;
+    },
+    setJpVar: function () {
+    	this.time = time;
+    	this.timeRemaining = timeRemaining;
+    }
+  }
+});
+var setUp = function(){
+	console.log('setup')
+	if(timeRemaining === '00:00'){
+		setTimeout(function(){ setUp() }, 50);
+	} else {
+		vm.setJpVar();
+	}
+}
+setUp();
